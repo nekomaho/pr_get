9e235abe375688fe5673294e0c5d1a0c4ef0eb22
nekomaho <nekosatoru@gmail.com>
2018-04-08 02:15:37 +0900

    Add PullRequest class
    can get all pull request number

--- lib/git_get.rb
+++ lib/git_get.rb
@@ -1,6 +1,16 @@
 require 'pry'
 require 'github_api'
 
-github = Github.new
+class PullRequest
+  def initialize(github, state='all')
+    @prs ||= github.pull_requests.list state: state
+  end
 
-p github.repos.list user: 'nekomaho'
+  def pr_numbers
+    @pr_numbers ||= @prs.map {|pr| pr[:number]}
+  end
+end
+
+prs = PullRequest.new(Github.new user: 'nekomaho', repo: 'dotfiles')
+p prs.pr_numbers
+
