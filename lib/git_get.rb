1695e07040efd9ff74c03da81ba34ec68fb9262e
nekomaho <nekosatoru@gmail.com>
2018-04-08 14:15:32 +0900

    Make PR Searcher from commit hash

--- lib/git_get.rb
+++ lib/git_get.rb
@@ -1,6 +1,21 @@
 require 'pry'
 require 'github_api'
 
-github = Github.new
+class Search
+  attr_reader :search_result
 
-p github.repos.list user: 'nekomaho'
+  def initialize(github, query)
+    @search_results ||= github.search.issues query
+  end
+
+  def urls
+    @search_results.body.items.map{ |result| result.html_url }
+  end
+end
+
+user = ARGV[0]
+repo = ARGV[1]
+commit_sha = ARGV[2]
+
+git = Github.new user: user, repo: repo, oauth_token: ENV['GIT_HUB_AUTH_TOKEN']
+puts Search.new(git, commit_sha).urls.each {|result| '#{result}\n'}
