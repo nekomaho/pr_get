1f6fad3aeaab79665e9e367bedac08bac5385d7d
nekomaho <nekosatoru@gmail.com>
2018-04-12 20:59:52 +0900

    Display PR title :tada:

--- lib/git_get.rb
+++ lib/git_get.rb
@@ -2,15 +2,36 @@ require 'pry'
 require 'github_api'
 require 'optparse'
 
-class Search
+class SearchItem
   attr_reader :search_result
 
   def initialize(github, query)
-    @search_results ||= github.search.issues query
+    @github = github
+    @query = query
   end
 
-  def urls
-    @search_results.body.items.map{ |result| result.html_url }
+  def exec
+    @github.search.issues(@query).body.items.map do |item|
+      ItemResult.new(item)
+    end
+  end
+end
+
+class ItemResult
+  def initialize(item)
+    @item = item
+  end
+
+  def number
+    @item.number
+  end
+
+  def url
+    @item.html_url
+  end
+
+  def title
+    @item.title
   end
 end
 
@@ -54,10 +75,12 @@ end
 input = CommandLineOption.new
 
 git = Github.new user: input.user, repo: input.repo, oauth_token: ENV['GIT_HUB_AUTH_TOKEN']
-urls = Search.new(git, input.sha).urls
+item_results = SearchItem.new(git, input.sha).exec.sort_by { |item| item.number }
 
 if input.disp_all?
-  puts urls.sort.each {|result| '#{result}\n'}
+  item_results.each do |item_result|
+    puts "#{item_result.url} : #{item_result.title}"
+  end
 else
-  puts urls.sort.first
+  puts "#{item_results.first.url} : #{item_results.first.title}"
 end
